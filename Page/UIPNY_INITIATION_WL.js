0bcd33dcf4ac99ab540d451a47dac1639e50159d
lmopapps <lmop.apps@gmail.com>
2016-11-08 04:08:58 +0700

    Revert "Create Github survey"
    This reverts commit 4a1f60bafb76492c9ff42449714b3edcc7e1ae5b.

--- Page/UIPNY_INITIATION_WL.js
+++
@@ -1,5 +0,0 @@
-$_OCT("#idBtnImport").click(
-	function(){
-		
-	}
-);
--- Page/UIPNY_ZONAAREA_IN.js
+++
@@ -1,6 +0,0 @@
-ReqKdPs = function(){
-	var PostCodeValue = $_OCT("#idKodePos").val();
-	var PostData = jQuery.pg.callDataschemeSync("T_LOSINITIATIONPOSTCODES","",PostCodeValue,"","","","");
-	if (PostData.length > 0) return true;
-	else return false;
-}
